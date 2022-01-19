3782521ec554a598ef7a67f7f421bc6fa83137eb
AnthonyLafolie <anthony59930@outlook.fr>
2022-01-19 08:07:17 +0100

    update temporary Page

--- lib/page_2.dart
+++ lib/page_2.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/src/provider.dart';
 import 'package:watch_it/authentication/bloc/authentication_bloc.dart';
+import 'package:watch_it/common/user_storage.dart';
 
 // ignore: must_be_immutable
-class Page2 extends StatelessWidget {
+class Page2 extends StatefulWidget {
   Color color;
   Page2({Key? key, required this.color}) : super(key: key);
 
@@ -15,14 +16,74 @@ class Page2 extends StatelessWidget {
   }
 
   @override
+  State<Page2> createState() => _Page2State();
+}
+
+class _Page2State extends State<Page2> {
+  String username = "";
+
+  String password = "";
+
+  String token = "";
+
+  bool rememberMe = false;
+
+  @override
+  void initState() {
+    super.initState();
+    UserStorage.getUsername().then((value) {
+      setState(() {
+        username = value!;
+      });
+    });
+    UserStorage.getPassword().then((value) {
+      setState(() {
+        password = value!;
+      });
+    });
+    UserStorage.getRememberMe().then((value) {
+      setState(() {
+        rememberMe = value!;
+      });
+    });
+    UserStorage.getToken().then((value) {
+      setState(() {
+        token = value!;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: color,
-      child: ElevatedButton(
-        child: const Text('Logout'),
-        onPressed: () {
-          context.read<AuthenticationBloc>().add(AuthenticationLogoutRequested());
-        },
+    return Scaffold(
+      body: Container(
+        color: widget.color,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Username : $username",
+            ),
+            Text(
+              "Password : $password",
+            ),
+            Text(
+              "Remember Me :  $rememberMe",
+            ),
+            Text(
+              "Token : $token",
+            ),
+            Center(
+              child: ElevatedButton(
+                child: const Text('Logout'),
+                onPressed: () async {
+                  context.read<AuthenticationBloc>().add(AuthenticationLogoutRequested());
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
