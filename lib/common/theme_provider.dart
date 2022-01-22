cb879b96c9b672d58c3066fd8b3da17e8c9ad8a8
AnthonyLafolie <anthony59930@outlook.fr>
2022-01-22 13:34:53 +0100

    add theme properties

--- lib/common/theme_provider.dart
+++ lib/common/theme_provider.dart
@@ -15,6 +15,14 @@ class Themes {
     colorScheme: const ColorScheme.dark(),
     appBarTheme: const AppBarTheme(
       backgroundColor: Colors.black,
+      iconTheme: IconThemeData(color: Colors.white),
+    ),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      backgroundColor: Colors.grey,
+      selectedIconTheme: IconThemeData(color: Colors.purple),
+      unselectedItemColor: Colors.white,
+      unselectedLabelStyle: TextStyle(color: Colors.white),
+      showSelectedLabels: true,
     ),
     iconTheme: const IconThemeData(color: Colors.white),
     fontFamily: 'Raleway',
@@ -24,13 +32,14 @@ class Themes {
     scaffoldBackgroundColor: Colors.white,
     colorScheme: const ColorScheme.light(),
     appBarTheme: const AppBarTheme(
-        backgroundColor: Colors.white,
-        iconTheme: IconThemeData(color: Colors.black)),
+      backgroundColor: Colors.white,
+      iconTheme: IconThemeData(color: Colors.black),
+    ),
     bottomNavigationBarTheme: const BottomNavigationBarThemeData(
       backgroundColor: Colors.black,
       selectedIconTheme: IconThemeData(color: Colors.purple),
       unselectedItemColor: Colors.white,
-      unselectedLabelStyle: TextStyle(color: Colors.red),
+      unselectedLabelStyle: TextStyle(color: Colors.white),
       showSelectedLabels: true,
     ),
     iconTheme: const IconThemeData(color: Colors.black),
